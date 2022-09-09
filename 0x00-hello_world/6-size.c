b54077ea084b88944a7a2b41dcbec7d17167ae93
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-08 21:06:58 -0700

    6-size.c writes a C program that prints the size of the various types of computer it is compiled on and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %d bytes\n", sizeof(char));
-	printf("Size of an int: %d bytes\n", sizeof(int));
-	printf("Size of a long int: %d bytes\n", sizeof(long int));
-	printf("Size of a long long int: %d bytes\n", sizeof(long long int));
-	printf("Size of a float: %d bytes\n", sizeof(float));
+	printf("Size of a char: %ld bytes\n", sizeof(char));
+	printf("Size of an int: %ld bytes\n", sizeof(int));
+	printf("Size of a long int: %li bytes\n", sizeof(long int));
+	printf("Size of a long long int: %1lu bytes\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes\n", sizeof(float));
 	return (0);
 }
