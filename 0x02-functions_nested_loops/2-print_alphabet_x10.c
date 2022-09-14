c3c519e11e582c3c863b32733c2a4c48bd38f7ee
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-14 10:48:17 -0700

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - prints 10 times the alphabet in lowercase
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int p;
+	int s;
+
+	for (s = 0; s < 10; s++)
+		for (p = 'a'; p <= 'z'; p++)
+			_putchar(p);
+		_putchar('\n');
+
+}
