0d817eb117128c6979f242a3ec7c753074417165
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-14 10:23:10 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description -> A function that prints the alphabet, in lowercas
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int p;
+
+	for (p = 'a', p <= 'z', p++)
+		_putchar(p);
+
+	_putchar('\n');
+
+}
