ebc43d472c0152dcbb194c12a0f20be78f4f9b5f
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-14 11:54:15 -0700

    Added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character
+ * @c: letter that is being tested
+ * Description - prints alphabetic character
+ * Return: Alway 0
+ */
+
+int _isalpha(int c)
+{
+	for (c = 65; c <= 90; c++ || c = 97; c <= 122; c++)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
