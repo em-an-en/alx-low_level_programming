944f70d09c69cc53f151a2d128f1ddc818fffc79
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-19 17:27:36 -0700

    Added 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * reset_to_98 - Set pointer param to 98.
- *   * @n: int type pointer
- *    */
+ * reset_to_98 - Set pointer param to 98.
+ * @n: int type pointer
+ */
 
 void reset_to_98(int *n)
 {
