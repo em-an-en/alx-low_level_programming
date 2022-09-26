7ebdda4078dbb8c68ed48c99429337f62064a16e
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-26 04:01:14 -0700

    Added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: string to be searched
+ * @accept: prefix substring
+ * Return: number of bytes in s
+ * that consists only that from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = o;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (bytes);
+			}
+		}
+		s++;
+	}
+
+	return (bytes);
+}
