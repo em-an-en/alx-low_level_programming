e004dac0ead8341e21660be5f13489a0fc21f09c
Ebube Ekeocha <maceyicon@gmail.com>
2022-09-26 18:50:58 -0700

    Added prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
+void set_string(char **s, char *to);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
