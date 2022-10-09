4a0ae87d7f3ce02e2775e067c369fdecf4adb88a
Ebube Ekeocha <111054722+em-an-en@users.noreply.github.com>
2022-10-09 16:56:08 +0100

    Added 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory like calloc
+ * @nmemb: number of members
+ * @size: size of each member
+ *
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int filler = 0, total = 0, i = 0;
+
+	char *p;
+	void *ptr;
+
+	total = size * nmemb;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (!ptr)
+		return (NULL);
+
+	p = ptr;
+
+	for (i = 0; i < total; i++)
+	{
+		p[i] = (char)filler;
+	}
+
+	return (ptr);
+}
