df21225e82d4ee346c7ebb8ece7a206c6ea3322a
Ebube Ekeocha <111054722+em-an-en@users.noreply.github.com>
2022-10-12 04:50:39 +0100

    Added 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include "stdlib.h"
+/**
+ * init_dog - initializes dog struct
+ * @d: pointer to dog object
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	}
+}
