f84a0e277b2a73fef860f4beac70195a113bcde0
Ebube Ekeocha <111054722+em-an-en@users.noreply.github.com>
2022-10-12 04:52:13 +0100

    Added 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+/**
+ * print_dog - prints dog member values
+ * @d: dog instance
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	double nilDouble = 0;
+
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (!d->age)
+			printf("Age: %f\n", nilDouble);
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
