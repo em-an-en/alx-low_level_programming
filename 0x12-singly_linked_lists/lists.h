279d66e14fe6720efa35564e46590ade81ebf5dc
Ebube Ekeocha <maceyicon@gmail.com>
2022-10-20 21:08:44 -0700

    Resolved betty issues

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,10 +6,10 @@
 #include <string.h>
 
 /**
- * struct list_s -
- * @str: 
- * @len: 
- * @next: 
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of string
+ * @next: points to the next node
  * Description: singly linked list node structure
  */
 typedef struct list_s
