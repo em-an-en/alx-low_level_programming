4d2ec610492a28dad30f2464b682416c8761bc1e
Ebube Ekeocha <111054722+em-an-en@users.noreply.github.com>
2022-10-30 00:12:43 +0100

    Added files

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int p;
+	unsigned long int q = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		p = q >> i;
+		if (p & 1)
+			count++;
+	}
+
+	return (count);
+}
